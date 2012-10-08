22c525f3c6afaa68fed5b275fca4d92895640d81
Thomas Girard <thomas.g.girard@free.fr>
2012-10-09 00:18:32 +0200

    fix df(1)
    it's now possible to invoke it like this:
    
      user> (closh.utils/df ["/" "/home" "/usr"])
    
    It would look way better without the vector part

--- src/closh/commands/df.clj
+++ src/closh/commands/df.clj
@@ -1,19 +1,26 @@
 ;; Implementation of the DF command
 (in-ns 'closh.utils)
 
-(import [java.nio.file FileSystems]
+(import [java.io File]
+        [java.nio.file Files]
         [java.net URI])
 
 (defn kb [bytes]
   (/ bytes 1024))
 
+(defn- df- [store]
+  ;; Filesystem Size Used Avail Use% Mounted on
+  (format "%s %d %d %d"
+          (.name store)
+          (kb (.getTotalSpace store))
+          (kb (- (.getTotalSpace store) (.getUnallocatedSpace store)))
+          (kb (.getUsableSpace store))))
+
 (defn df
-  ([] (df "/"))
-  ([path]
-     (let [fs (FileSystems/getFileSystem (URI. (str "file://" path)))]
-       (str/join \newline
-                 (map #(format "%s %d %d"
-                               (.name %)
-                               (kb (.getUnallocatedSpace %))
-                               (kb (.getTotalSpace %)))
-                      (.getFileStores fs))))))
+  ([] (df ["/"]))
+  ([paths]
+     (do
+       (println "Filesystem Size Used Avail")
+       (println
+        (str/join \newline
+                  (map #(df- (Files/getFileStore (.toPath (File. %)))) paths))))))
